4b9ea677da2e4a531651c11ab31815cd408514ea
denis-md <dmalashin@gmail.com>
2020-05-06 17:52:01 +0300

    safari fix

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -77,23 +77,23 @@ module.exports = {
           {
             loader: "image-webpack-loader",
             options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75,
-              },
+              // mozjpeg: {
+              //   progressive: true,
+              //   quality: 65,
+              // },
+              // optipng: {
+              //   enabled: true,
+              // },
+              // pngquant: {
+              //   quality: [0.65, 0.9],
+              //   speed: 4,
+              // },
+              // gifsicle: {
+              //   interlaced: false,
+              // },
+              // webp: {
+              //   quality: 75,
+              // },
             },
           },
         ],
--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import "./main.scss";
-import heroImageSrc from './assets/images/Hero_BG_Image.jpg'
+import heroImageSrc from "./assets/images/Hero_BG_Image.jpg";
 import bigFeatureSrc from "./assets/images/Big_Feature_Image.png";
-import quoteImageSrs from "./assets/images/Quote_Image.png";
+import quoteImageSrc from "./assets/images/Quote_Image.png";
 
-const imageSetter = () => {
-  document.querySelector(".review__image").src = quoteImageSrs;
-  document.querySelector(".about__image").src = bigFeatureSrc;
-  document.querySelector(".header").style.background = `url('${heroImageSrc}')`;
-};
+// const imageSetter = () => {
+//   console.log(heroImageSrc, bigFeatureSrc, quoteImageSrc);
+//   document.querySelector(".review__image").src = quoteImageSrc;
+//   document.querySelector(".about__image").src = bigFeatureSrc;
+//   document.querySelector(".header").style.background = `url('${heroImageSrc}')`;
+// };
 
-imageSetter()
+// imageSetter();
