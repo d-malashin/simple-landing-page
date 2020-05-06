40163e10205c1c20c8bcd87b612b5060612fe9ee
denis-md <dmalashin@gmail.com>
2020-05-06 18:00:15 +0300

    safari fix2

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import heroImageSrc from "./assets/images/Hero_BG_Image.jpg";
 import bigFeatureSrc from "./assets/images/Big_Feature_Image.png";
 import quoteImageSrc from "./assets/images/Quote_Image.png";
 
-// const imageSetter = () => {
-//   console.log(heroImageSrc, bigFeatureSrc, quoteImageSrc);
-//   document.querySelector(".review__image").src = quoteImageSrc;
-//   document.querySelector(".about__image").src = bigFeatureSrc;
-//   document.querySelector(".header").style.background = `url('${heroImageSrc}')`;
-// };
+const imageSetter = () => {
+  console.log(heroImageSrc, bigFeatureSrc, quoteImageSrc);
+  document.querySelector(".review__image").src = quoteImageSrc;
+  document.querySelector(".about__image").src = bigFeatureSrc;
+  document.querySelector(".header").style.background = `url('${heroImageSrc}')`;
+};
 
-// imageSetter();
+imageSetter();
